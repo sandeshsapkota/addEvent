a545b3791740d4680ad40bd3b58feebcb6bb7429
Sandesh Sapkota <sapkotasubas11@gmail.com>
2019-12-24 21:26:57 +0545

    unequal borderline fixed to equal

--- day-3.js
+++ day-3.js
@@ -1,39 +1,34 @@
 const button = document.querySelector(".button");
-const {
-  x: buttonX,
-  y: buttonY,
-  width,
-  height
-} = button.getBoundingClientRect();
+let { width, height, x: buttonX, y: buttonY } = button.getBoundingClientRect();
+
+buttonX = buttonX + width / 2; // horizontal x of
+buttonY = buttonY + height / 2;
 
 /*************** Functions ***************/
 
-let xLeft, xRight, yTop, yBottom, mouseIsInButtonTerritory;
+let distance = width;
+let mouseEntered = true;
 
 function mouseMove(e) {
-  const x = e.x;
-  const y = e.y;
-   xLeft = buttonX - width;
-   xRight = buttonX + width + width;
-   yTop = buttonY - height;
-   yBottom = buttonY + height + height;
+  const x = e.x; // current x of cursor
+  const y = e.y; // current y of cursor
+  const xLeft = buttonX - distance;
+  const xRight = buttonX + distance;
+  const yTop = buttonY - distance;
+  const yBottom = buttonY + distance;
 
-  mouseIsInButtonTerritory = x > xLeft && x < xRight && y > yTop && y < yBottom;
+  const mouseIsInButtonTerritory =
+    x > xLeft && x < xRight && y > yTop && y < yBottom;
 
   const xWalk = (x - buttonX) / 2;
   const yWalk = (y - buttonY) / 2;
 
   if (mouseIsInButtonTerritory) {
-    console.log(xLeft)
-
-    xLeft = xLeft - 40;
-    xRight = xRight + 40;
-    yTop = yTop - 40;
-    yBottom = yBottom + 40;
-    console.log(xLeft)
+    if (mouseEntered === true) {
+      distance = distance + distance;
+      mouseEntered = false;
+    }
     catchCursor(xWalk, yWalk);
-  mouseIsInButtonTerritory = x > xLeft && x < xRight && y > yTop && y < yBottom;
-
   } else {
     resetPositon();
   }
@@ -45,6 +40,7 @@ function catchCursor(xWalk, yWalk) {
 
 function resetPositon() {
   button.style.transform = `translate(${0}px, ${0}px)`;
+  mouseEntered = true;
 }
 
 /*************** Event-handler ***************/
